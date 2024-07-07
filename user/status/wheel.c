d857fa0b4b98d85b35be6bdfc0a9f7dcc1740bf8
xj63 <xj63work@163.com>
2024-07-07 16:17:33 +0800

    fix: motor init.

--- user/status/wheel.c
+++ user/status/wheel.c
@@ -39,6 +39,9 @@ void status_wheel_init(struct Wheel wheels[WHEEL_NUMS],
 }
 
 void status_wheels_init(struct Wheel wheels[WHEEL_NUMS]) {
+  INFO("motor init.");
+  motor_init();
+
   status_wheel_init(wheels, FONT_LEFT, 2, 0, 0, 5, 50);
   status_wheel_init(wheels, FONT_RIGHT, 2, 1, 0, 5, 50);
   status_wheel_init(wheels, BACK_LEFT, 1, 1, 1, 5, 50);
