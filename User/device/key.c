bb8a8392e84468e6c678bad0e62a9cc8213884ea
xj63 <xj63work@163.com>
2024-07-17 13:59:55 +0800

    feat: key callback use closure

--- User/device/key.c
+++ User/device/key.c
@@ -19,32 +19,33 @@ void key2_callback() {
 bool key1_is_press() { return !DL_GPIO_readPins(KEY_PORT, KEY_KEY1_PIN); }
 bool key2_is_press() { return !DL_GPIO_readPins(KEY_PORT, KEY_KEY2_PIN); }
 
-void task_timed_key_react_new(void (*key_react)(void *), unsigned later) {
-  Task t = task_new(key_react, NULL);
+void task_timed_key_react_new(void (*key_react)(void *), void *para,
+                              unsigned later) {
+  Task t = task_new(key_react, para);
   task_timed_append(&task.timed, t, later);
 }
 
-void key1_react(void *_) {
+void key1_react(void *callback) {
   if (key1_is_press()) {
-    key1_callback();
-    task_timed_key_react_new(key1_react, TIMER1_FREQ);
+    ((void (*)())callback)();
+    task_timed_key_react_new(key1_react, callback, TIMER1_FREQ);
     return;
   }
 
-  task_timed_key_react_new(key1_react, 1);
+  task_timed_key_react_new(key1_react, callback, 1);
 }
 
-void key2_react(void *_) {
+void key2_react(void *callback) {
   if (key2_is_press()) {
-    key2_callback();
-    task_timed_key_react_new(key2_react, TIMER1_FREQ);
+    ((void (*)())callback)();
+    task_timed_key_react_new(key2_react, callback, TIMER1_FREQ);
     return;
   }
 
-  task_timed_key_react_new(key2_react, 1);
+  task_timed_key_react_new(key2_react, callback, 1);
 }
 
 void keyreact_init() {
-  key1_react(NULL);
-  key2_react(NULL);
+  key1_react(key1_callback);
+  key2_react(key2_callback);
 }
