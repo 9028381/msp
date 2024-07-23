51aa4f8da3fc162ee988f342fc5ac6729d4060dc
xj63 <xj63work@163.com>
2024-07-23 21:05:56 +0800

    fix: test right

--- User/status/status.c
+++ User/status/status.c
@@ -104,8 +104,9 @@ void status_next(struct Status *sta) {
     int delta;
     switch (sta->sensor.follow) {
     case ROAD_LEFT:
-      sta->wheels[FONT_LEFT].target = MAX_FOLLOW_TURN_SPEED;
-      sta->wheels[FONT_RIGHT].target = -MAX_FOLLOW_TURN_SPEED;
+      sta->wheels[FONT_LEFT].target = -MAX_FOLLOW_TURN_SPEED;
+      sta->wheels[FONT_RIGHT].target = MAX_FOLLOW_TURN_SPEED;
+      break;
     case ROAD_RIGHT:
       sta->wheels[FONT_LEFT].target = -MAX_FOLLOW_TURN_SPEED;
       sta->wheels[FONT_RIGHT].target = MAX_FOLLOW_TURN_SPEED;
