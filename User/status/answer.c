8867e885b82aef79c31906647523b4327a2fa814
xj63 <xj63work@163.com>
2024-07-29 11:31:17 +0800

    feat: answer4

--- User/status/answer.c
+++ User/status/answer.c
@@ -26,3 +26,15 @@ void answer3(struct Status *sta) {
   step_push(&sta->step, action_follow, condition_roadless);
   step_push(&sta->step, action_stop, condition_never);
 }
+
+void answer4(struct Status *sta) {
+  step_clear(&sta->step);
+  for (int i = 0; i < 4; i++) {
+    step_push(&sta->step, action_keep_0, condition_findline);
+    step_push(&sta->step, action_follow, condition_roadless);
+    step_push(&sta->step, action_turn_to_104, condition_turn_to);
+    step_push(&sta->step, action_keep_104, condition_findline);
+    step_push(&sta->step, action_follow, condition_roadless);
+  }
+  step_push(&sta->step, action_stop, condition_never);
+}
