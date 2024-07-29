9e67a53e23a85076a49b043a9c10fe9868c9207c
xj63 <xj63work@163.com>
2024-07-29 11:43:38 +0800

    fix: answer 3,4 turn to 284

--- User/status/answer.c
+++ User/status/answer.c
@@ -21,8 +21,8 @@ void answer3(struct Status *sta) {
   step_clear(&sta->step);
   step_push(&sta->step, action_keep_0, condition_findline);
   step_push(&sta->step, action_follow, condition_roadless);
-  step_push(&sta->step, action_turn_to_104, condition_turn_to);
-  step_push(&sta->step, action_keep_104, condition_findline);
+  step_push(&sta->step, action_turn_to_284, condition_turn_to);
+  step_push(&sta->step, action_keep_284, condition_findline);
   step_push(&sta->step, action_follow, condition_roadless);
   step_push(&sta->step, action_stop, condition_never);
 }
@@ -32,8 +32,8 @@ void answer4(struct Status *sta) {
   for (int i = 0; i < 4; i++) {
     step_push(&sta->step, action_keep_0, condition_findline);
     step_push(&sta->step, action_follow, condition_roadless);
-    step_push(&sta->step, action_turn_to_104, condition_turn_to);
-    step_push(&sta->step, action_keep_104, condition_findline);
+    step_push(&sta->step, action_turn_to_284, condition_turn_to);
+    step_push(&sta->step, action_keep_284, condition_findline);
     step_push(&sta->step, action_follow, condition_roadless);
   }
   step_push(&sta->step, action_stop, condition_never);
