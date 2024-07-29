f6a366d316065cb54aad7e02706a3681617799c8
xj63 <xj63work@163.com>
2024-07-29 19:39:38 +0800

    feat: answer keep angle with limit

--- User/status/answer.c
+++ User/status/answer.c
@@ -42,10 +42,9 @@ void answer1(struct Status *sta) {
 
 void answer2(struct Status *sta) {
   step_clear(&sta->step);
-  step_push(&sta->step, action_keep_0, condition_findline);
+  step_push(&sta->step, action_keep_0, condition_findline_with_60000_70000_history_limit);
   step_push(&sta->step, action_follow, condition_roadless);
-//   step_push(&sta->step, action_turn_to_180, condition_turn_to);
-  step_push(&sta->step, action_keep_180, condition_findline);
+  step_push(&sta->step, action_keep_180, condition_findline_with_60000_70000_history_limit);
   step_push(&sta->step, action_follow, condition_roadless);
   step_push(&sta->step, action_stop, condition_never);
 }
